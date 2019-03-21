7798611961b8aacb48c4863df056218742793f6f
Ethan <N/A>
2019-03-21 14:58:29 -0700

    COmmit 1 - Verbal Questions

---
+++ js/app.js
@@ -0,0 +1,9 @@
+{
+    //1. A parameter is the variable you use when making the function
+    //an argument is what you pass to the function
+    //2. console.log just shows a message in the console
+    //return returns some form of data to be used
+    //3. you can use a function to manipulate or check data, then manipulate it further.
+    // allows you to write it once and use it multiple times
+}
+
